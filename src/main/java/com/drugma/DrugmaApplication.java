3f988ba1e17c75838d14da32178d84cecf35111f
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-06 22:56:34 +0200

    initial commit

---
+++ src/main/java/com/drugma/DrugmaApplication.java
@@ -0,0 +1,12 @@
+package com.drugma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugmaApplication.class, args);
+	}
+}
