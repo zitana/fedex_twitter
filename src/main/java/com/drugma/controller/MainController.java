f1531308e6bd92324df611a36cab58daad0bc402
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-07 13:41:44 +0200

    remove unnecessary lines

--- src/main/java/com/drugma/controller/MainController.java
+++ src/main/java/com/drugma/controller/MainController.java
@@ -2,13 +2,9 @@ package com.drugma.controller;
 
 import com.drugma.model.HearthbeatRequest;
 import com.drugma.model.Status;
-import com.drugma.model.URLConnectionReader;
-import java.util.concurrent.TimeUnit;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import twitter4j.Twitter;
@@ -16,8 +12,6 @@ import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.OAuthAuthorization;
 import twitter4j.conf.ConfigurationContext;
-import java.net.*;
-import java.io.*;
 
 @RestController
 public class MainController {
@@ -25,8 +19,6 @@ public class MainController {
   @RequestMapping(value = "/{tweet}", method = RequestMethod.GET)
   public Status receive(@PathVariable("tweet") String tweet) throws Exception {
     new RestTemplate().postForObject("https://natural-radar.glitch.me/hearthbeat", new HearthbeatRequest("GITHUB_BUILD", "STARTED"), HearthbeatRequest.class);
-    // TimeUnit.SECONDS.sleep(3);
-    // String tweet = new URLConnectionReader().getText("https://raw.githubusercontent.com/gy0p4k/fedex/master/content");
     OAuthAuthorization authorization = new OAuthAuthorization(ConfigurationContext.getInstance());
     Twitter twitter = new TwitterFactory().getInstance(authorization);
     try {
