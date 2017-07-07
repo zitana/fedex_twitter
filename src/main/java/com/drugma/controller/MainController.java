faca765cdc478662a836eb8e1a95d934b5b31516
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-07-07 13:30:38 +0200

    import pathvariable

--- src/main/java/com/drugma/controller/MainController.java
+++ src/main/java/com/drugma/controller/MainController.java
@@ -5,6 +5,7 @@ import com.drugma.model.Status;
 import com.drugma.model.URLConnectionReader;
 import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
